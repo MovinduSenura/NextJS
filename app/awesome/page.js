63c1adba93387eb1d1b6aad54c14707c4bd729c4
MovinduSenura <it22371218@my.sliit.lk>
2024-09-18 14:12:50 +0530

    second commit

---
+++ app/awesome/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./page.css"
+
+export default function page() {
+  return (
+    <main>
+        <h1 className='first'>NextJS is awesome!</h1>
+    </main>
+  )
+}
